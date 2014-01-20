183da6c8dc154250fab8d1bbd8f7842da3d81ccf
bvincent1 <vincent@ualberta.ca>
2014-01-20 11:46:17 -0700

    Compiled under lab machines

--- ClickerCounter/src/com/example/clickercounter/ClickerCounterMain.java
+++ ClickerCounter/src/com/example/clickercounter/ClickerCounterMain.java
@@ -4,18 +4,18 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class ClickerCounter extends Activity {
+public class ClickerCounterMain extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_clicker_counter);
+		setContentView(R.layout.activity_clicker_counter_main);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.clicker_counter, menu);
+		getMenuInflater().inflate(R.menu.clicker_counter_main, menu);
 		return true;
 	}
 
