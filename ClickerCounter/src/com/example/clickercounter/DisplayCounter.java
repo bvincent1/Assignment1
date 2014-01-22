4be27e3979c377bc48faa2cc80ca3ae4bc90e00f
bvincent1 <vincent@ualberta.ca>
2014-01-22 14:58:48 -0700

    Added write and read functions

--- ClickerCounter/src/com/example/clickercounter/DisplayCounter.java
+++ ClickerCounter/src/com/example/clickercounter/DisplayCounter.java
@@ -1,6 +1,14 @@
 package com.example.clickercounter;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,10 +16,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.gson.Gson;
+
 public class DisplayCounter extends Activity {
 
     public final static String EXTRA_MESSAGE = "com.example.CLickerCounter.MESSAGE";
 	public ClickerCounterModel clickerCountObject;
+	public final String filename = "ClickerCounter.sav"; 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +37,15 @@ public class DisplayCounter extends Activity {
 		// TODO add file check to see if we need a new name or we are searching for an existing one
 		// get name and check to see if we need to generate new name or import old one
 		
+		ClickerCounterModel[] temp = makeClickerModelArray(3);
+		System.out.println(temp[0].getClickerName());
+		Gson gson = new Gson();
+		String input = gson.toJson(temp);
+		
 		clickerCountObject = new ClickerCounterModel(tempCounterName);
 		
 		EditText clickerName = (EditText) findViewById(R.id.clikerCounterName);
 		clickerName.setHint(clickerCountObject.getClickerName());
-		
 	}
 
 	@Override
@@ -66,12 +81,51 @@ public class DisplayCounter extends Activity {
 	    startActivity(intent);
 	}
 	
-	public ClickerCounterModel[] makeObjectArray(){
-		ClickerCounterModel[] tempObject = new ClickerCounterModel[3];
-		for(int i = 0; i < 3 + 1; i++){
-			tempObject[i].setClickerName("test"+i);
+	public ClickerCounterModel[] makeClickerModelArray(int num){
+		// make array of clicker counter model objects
+		ClickerCounterModel[] tempObject = new ClickerCounterModel[num];
+		for(int i = 0; i < num; i++){
+			tempObject[i] = new ClickerCounterModel("test"+i);
+			tempObject[i].setClickerCount(i);
 		}
 		return tempObject;
-		
+	}
+
+	public ClickerCounterModel readObjectFromFile(){
+		// read in ClickerCounterModel gson type from file
+		StringBuilder sb = new StringBuilder();
+		try{
+			FileInputStream inputStream = openFileInput(filename);
+			InputStreamReader isr = new InputStreamReader(inputStream);
+			BufferedReader bufferedReader = new BufferedReader(isr);
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				sb.append(line);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// use Gson to convert it into and object
+		String json = sb.toString();
+		Gson gson = new Gson();
+		ClickerCounterModel object = gson.fromJson(json, ClickerCounterModel.class);
+		return object;
+	}
+
+	public void writeToFile(ClickerCounterModel temp){
+		// convert from object to gson and write to file
+		try {
+			Gson gson = new Gson();
+			String string = gson.toJson(temp);
+			FileOutputStream outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+			outputStream.write(string.getBytes());
+			outputStream.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
